c109682590ea58440293bbfe2289166ed112d618
teyim <teyimasobo@gmail.com>
2025-01-30 09:44:18 +0100

    feat: delete uploaded aws image if bacground already exist in db

--- apps/api/src/modules/background/data-access.ts
+++ apps/api/src/modules/background/data-access.ts
@@ -1,22 +1,45 @@
 import { PrismaClient, Template } from "@prisma/client";
 import { Background } from "@prisma/client";
 import { getDataAccessErrorMessage } from "../../utils/errors";
+import { ENV_variables, HttpStatusCode } from "../../constants";
+import { getS3Key, deleteS3Object } from "../../utils";
+import { ErrorWithStatus } from "../../types/error";
 
 const prisma = new PrismaClient();
 
 export const addBackground = async (
   name: string,
   imgUrl: string,
-  recommendedColors: string
+  recommendedColors: string,
 ) => {
   try {
+    const existingBackground = await prisma.background.findUnique({
+      where: { name },
+    });
+
+    if (!!existingBackground) {
+      // Extract S3 key from image URL
+      const key = getS3Key(
+        existingBackground.imageUrl,
+        ENV_variables.AWS_S3_BUCKET,
+      );
+
+      // Delete from S3 and database
+      await deleteS3Object(ENV_variables.AWS_S3_BUCKET, key);
+
+      throw new ErrorWithStatus(
+        `Background with name: ${existingBackground.name} already exist`, HttpStatusCode.Conflict
+      );
+    }
     // Validate and parse recommendedColors
     let parsedColors = {};
     if (recommendedColors) {
       try {
         parsedColors = JSON.parse(recommendedColors);
-      } catch (parseError:any) {
-        throw new Error(`Invalid recommendedColors format: ${parseError?.message}`);
+      } catch (parseError: any) {
+        throw new ErrorWithStatus(
+          `Invalid recommendedColors format: ${parseError?.message}`, HttpStatusCode.BadRequest
+        );
       }
     }
 
@@ -31,14 +54,16 @@ export const addBackground = async (
     });
     return background;
   } catch (error) {
-    if(error instanceof Error) {
+    if (error instanceof ErrorWithStatus) {
       if (error.message.startsWith("Background with name")) {
         console.error(`Duplicate entry: ${error.message}`);
-        throw error;
+        throw new ErrorWithStatus(error?.message, error.status)
       }
       if (error.message.startsWith("Invalid recommendedColors")) {
         console.error(`JSON Parse Error: ${error.message}`);
-        throw new Error("Invalid color format. Please provide valid JSON format for recommended colors");
+        throw new Error(
+          "Invalid color format. Please provide valid JSON format for recommended colors",
+        );
       }
     }
 
@@ -46,18 +71,17 @@ export const addBackground = async (
     console.error(getDataAccessErrorMessage("background", "create"), error);
     throw new Error(getDataAccessErrorMessage("background", "create"));
   }
-
 };
 
 export const getBackgroundByIdWithTemplates = async (
-  id: string
+  id: string,
 ): Promise<(Background & { templates: Template[] }) | null> => {
   try {
     const background = await prisma.background.findUnique({
       where: { id },
       include: { templates: true },
     });
-    return background
+    return background;
   } catch (error) {
     console.error(getDataAccessErrorMessage("background", "get"), error);
     throw new Error(getDataAccessErrorMessage("background", "get"));
--- apps/api/src/modules/background/service.ts
+++ apps/api/src/modules/background/service.ts
@@ -1,6 +1,9 @@
 import { addBackground } from "./data-access";
 import { ErrorWithStatus } from "../../types/error";
-import { getBackgroundByIdWithTemplates, deleteBackgroundById } from "./data-access";
+import {
+  getBackgroundByIdWithTemplates,
+  deleteBackgroundById,
+} from "./data-access";
 
 import { ENV_variables, ErrorMessages, HttpStatusCode } from "../../constants";
 import { handleError } from "../../utils/errors";
@@ -9,29 +12,34 @@ import { deleteS3Object, getS3Key } from "../../utils";
 export const addBackgroundService = async (
   name: string,
   imgUrl: string,
-  recommendedColors: any
+  recommendedColors: any,
 ) => {
   try {
     const background = await addBackground(name, imgUrl, recommendedColors);
     return background;
   } catch (error) {
-    handleError(error)
+    handleError(error);
   }
 };
-
 
 export const deleteBackgroundService = async (id: string): Promise<void> => {
   try {
     // Check if background exists and get associated templates
     const background = await getBackgroundByIdWithTemplates(id);
-    console.log(background)
+    console.log(background);
     if (!background) {
-      throw new ErrorWithStatus(ErrorMessages.BACKGROUND.NOT_FOUND, HttpStatusCode.NotFound);
+      throw new ErrorWithStatus(
+        ErrorMessages.BACKGROUND.NOT_FOUND,
+        HttpStatusCode.NotFound,
+      );
     }
 
     // Prevent deletion if templates exist
     if (background.templates.length > 0) {
-      throw new ErrorWithStatus(ErrorMessages.BACKGROUND.DELETE_WITH_TEMPLATES, HttpStatusCode.BadRequest);
+      throw new ErrorWithStatus(
+        ErrorMessages.BACKGROUND.DELETE_WITH_TEMPLATES,
+        HttpStatusCode.BadRequest,
+      );
     }
 
     // Extract S3 key from image URL
@@ -40,10 +48,11 @@ export const deleteBackgroundService = async (id: string): Promise<void> => {
     // Delete from S3 and database
     await deleteS3Object(ENV_variables.AWS_S3_BUCKET, key);
     await deleteBackgroundById(id);
-  } catch (error:any) {
+  } catch (error: any) {
     if (error instanceof ErrorWithStatus) throw error;
-    throw new ErrorWithStatus("Error deleting background: " + error.message, 500);
+    throw new ErrorWithStatus(
+      "Error deleting background: " + error.message,
+      500,
+    );
   }
 };
-
-
